7f19e611e0d1cab49e581ce2e8b8f9977873a72d
aussenseiter13 <Carefully5693@proton.me>
2023-12-29 15:31:19 +0100

    Finished episode

--- first-vue-project/src/views/HomeView.vue
+++ first-vue-project/src/views/HomeView.vue
@@ -1,9 +1,18 @@
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
+import { ref } from 'vue'
+
+let message = ref('Hello World')
+
+setTimeout(() => {
+  message.value = 'Changes'
+}, 2000)
 </script>
 
 <template>
   <main>
     <TheWelcome />
+    <p>{{ message }}</p>
+    <input type="text" v-model="message" />
   </main>
 </template>
